908b76d9e4b869bd847e2361d7c8816ae13dbf40
Shahim <muhdshahim786@gmail.com>
2023-09-18 16:47:53 +0400

    added full CURD operation

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,9 +14,55 @@ const createUser = async (req, res) => {
 const listUsers = async (req, res) => {
   try {
     const users = await User.findAll();
-    res.status(201).json(users);
+    res.status(200).json(users);
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id, name, email } = req.body;
+
+    // Find the user by ID
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user attributes
+    user.name = name;
+    user.email = email;
+
+    // Save the changes to the database
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  try {
+    const { id } = req.query; // Get the ID from request parameters
+
+    // Find the user by ID
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Delete the user
+    await user.destroy();
+
+    res.status(204).json(); // 204 No Content for successful deletion
+  } catch (error) {
+    console.error("Error deleting user:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -24,4 +70,6 @@ const listUsers = async (req, res) => {
 module.exports = {
   createUser,
   listUsers,
+  updateUser,
+  deleteUserById,
 };
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { createUser, listUsers } = require("../controllers/userController");
+const {
+  createUser,
+  listUsers,
+  updateUser,
+  deleteUserById,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/users", createUser);
 router.get("/users", listUsers);
+router.put("/users", updateUser);
+router.delete("/users", deleteUserById);
 
 module.exports = router;
